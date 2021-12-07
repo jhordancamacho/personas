98dcd3726ee156ba8e24ff7fbe0d8e7d493fbac4
jhordancamacho <jhordan.camacho@gmail.com>
2021-12-07 10:33:57 -0500

    primer commit

---
+++ src/main/java/com/gestionpersonas/PersonasApplication.java
@@ -0,0 +1,13 @@
+package com.gestionpersonas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonasApplication.class, args);
+	}
+
+}
